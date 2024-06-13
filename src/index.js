7e795b16418922c135d221117ec47a6f13de9352
ayeuhugyu <72027312+ayeuhugyu@users.noreply.github.com>
2024-06-13 22:07:50 +0000

    untested wip website

--- src/index.js
+++ src/index.js
@@ -3,6 +3,40 @@ dotenv.config();
 import { ShardingManager } from "discord.js";
 import * as log from "./lib/log.js";
 import process from "node:process"
+import express from "express";
+import fs from "fs"
+
+const app = express();
+const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+const webRootPath = "./src/WebServer";
+
+const rootPath = "./src/WebServer";
+
+app.use(express.static(rootPath));
+
+app.get("/", (request, response) => {
+    log.debug("GET /")
+    return response.sendFile(`${rootPath}/index.html`, { root: rootPath });
+});
+
+app.listen(config.WebServer.port, () =>
+    log.info(`app listening at http://localhost:${config.WebServer.port} (@home network = http://192.168.4.31:${config.WebServer.port})`)
+);
+
+app.use((err, req, res, next) => {
+    log.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.get('/read-statistics-file', (req, res) => {
+    fs.readFile('./resources/data/statistics.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error reading file');
+        }
+        res.send(data);
+    });
+});
 
 log.debug("starting pepperbot");
 
