693f3e9236045af9466b01aa51f9047fcceed6b3
ayeuhugyu <72027312+ayeuhugyu@users.noreply.github.com>
2025-02-10 17:06:56 +0000

    untested p/restart

---
+++ src/commands/restart.ts
@@ -0,0 +1,52 @@
+import { Collection, Message } from "discord.js";
+import { Command, CommandAccess, CommandCategory, CommandOption, CommandOptionType, CommandResponse } from "../lib/classes/command";
+import * as action from "../lib/discord_action";
+
+const command = new Command(
+    {
+        name: 'restart',
+        description: 'restarts the given process',
+        category: CommandCategory.Debug,
+        pipable_to: [],
+        example_usage: "p/restart sharder",
+        aliases: [],
+        access: new CommandAccess({
+            users: ["440163494529073152"]
+        }),
+        options: [
+            new CommandOption({
+                name: 'process',
+                description: 'the process to restart',
+                required: false,
+                type: CommandOptionType.String
+            })
+        ]
+    }, 
+    async function getArguments ({ self, message, guildConfig }) {
+        message = message as Message;
+        const args = new Collection();
+        const commandLength = `${guildConfig.other.prefix}${self.name}`.length;
+        const prcs = message.content.slice(commandLength)?.trim(); // short for process because process is a reserved word
+        args.set('process', prcs);
+        return args;
+    },
+    async function execute ({ message, args }) {
+        if (!args?.get("process")) {
+            args?.set("process", "sharder");
+        }
+        if (args?.get("process") === "sharder" || args?.get("process") === "site") {
+            fetch("http://localhost:50000/restart", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ process: args?.get("process") })
+            }).then(async (response) => {
+                const content = await response.text();
+                action.reply(message, content);
+            })
+        }
+    }
+);
+
+export default command;
