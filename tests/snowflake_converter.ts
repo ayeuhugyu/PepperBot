89bc4741275a49c8d7ddf67e361bd05948aa44a9
ayeuhugyu <72027312+ayeuhugyu@users.noreply.github.com>
2025-02-20 16:37:25 +0000

    removed p/snowflake it was lowkey dumb. its functionality is still in the tests/snowflake_converter.ts thing though

--- tests/snowflake_converter.ts
+++ tests/snowflake_converter.ts
@@ -1,20 +1,20 @@
 const id = "1077030283792097420";
 
-let binary = Number(id).toString(2); 
+let binary = Number(id).toString(2);
 if (binary.length < 64) {
     binary = ("0".repeat(64 - binary.length) + binary);
-} 
-const timestampBinary = binary.slice(0, 42); 
+}
+const timestampBinary = binary.slice(0, 42);
 const timestamp = (parseInt(timestampBinary, 2) + 1420070400000); // add discord epoch
-const date = new Date(timestamp); 
+const date = new Date(timestamp);
 
 const workerIdBinary = binary.slice(43, 48);
 const processIdBinary = binary.slice(49, 54);
-const incrementBinary = binary.slice(55, 64); 
+const incrementBinary = binary.slice(55, 64);
 
 const workerId = parseInt(workerIdBinary, 2);
 const processId = parseInt(processIdBinary, 2);
 const increment = parseInt(incrementBinary, 2);
 
-const string = `full binary: ${binary}\ntimestamp binary: ${timestampBinary}\nworker id binary: ${workerIdBinary}\nprocess id binary: ${processIdBinary}\nincrement binary: ${incrementBinary}\ntimestamp: ${timestamp}\ndate: ${date}\nworker id: ${workerId}\nprocess id: ${processId}\nincrement: ${increment}`; 
-console.log(`input id: ${id}\n\noutput: \n\n${string}`);
+const string = `input id: ${id}\n\nfull binary: ${binary}\ntimestamp binary: ${timestampBinary}\nworker id binary: ${workerIdBinary}\nprocess id binary: ${processIdBinary}\nincrement binary: ${incrementBinary}\ntimestamp: ${timestamp}\n\ndate: ${date}\nworker id: ${workerId}\nprocess id: ${processId}\nincrement: ${increment}`;
+console.log(string);
