6a6c4a727014f15e9284e30525f98a5fedf181e4
ayeuhugyu <72027312+ayeuhugyu@users.noreply.github.com>
2025-03-20 16:54:54 +0000

    PagedMenu

---
+++ src/lib/classes/pagination.ts
@@ -0,0 +1,66 @@
+import { Message, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, Interaction, InteractionResponse } from 'discord.js';
+
+class PagedMenu {
+    private embeds: EmbedBuilder[];
+    private currentPage: number;
+    private activeMessage: Message | null;
+
+    constructor(embeds: EmbedBuilder[]) {
+        this.embeds = embeds;
+        this.currentPage = 0;
+        this.activeMessage = null;
+    }
+
+    getActionRow(): ActionRowBuilder<ButtonBuilder> {
+        return new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('previous')
+                    .setLabel('Previous')
+                    .setStyle(ButtonStyle.Primary)
+                    .setDisabled(this.currentPage === 0),
+                new ButtonBuilder()
+                    .setCustomId('next')
+                    .setLabel('Next')
+                    .setStyle(ButtonStyle.Primary)
+                    .setDisabled(this.currentPage === this.embeds.length - 1)
+            );
+    }
+
+    public async setActiveMessage(message: Message): Promise<void> {
+        this.activeMessage = message;
+        await this.updateMessage();
+
+        const collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60000 });
+
+        collector.on('collect', async (interaction: Interaction) => {
+            if (!interaction.isButton()) return;
+
+            if (interaction.customId === 'previous' && this.currentPage > 0) {
+                this.currentPage--;
+            } else if (interaction.customId === 'next' && this.currentPage < this.embeds.length - 1) {
+                this.currentPage++;
+            }
+
+            await this.updateMessage();
+            await interaction.deferUpdate();
+        });
+
+        collector.on('end', () => {
+            if (this.activeMessage) {
+                this.activeMessage.edit({ components: [] });
+            }
+        });
+    }
+
+    private async updateMessage(): Promise<void> {
+        if (this.activeMessage) {
+            await this.activeMessage.edit({
+                embeds: [this.embeds[this.currentPage]],
+                components: [this.getActionRow()]
+            });
+        }
+    }
+}
+
+export default PagedMenu;
