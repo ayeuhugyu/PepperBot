1c1d3c7bc608b6d9b861bba20879a5352fe01fbd
ayeuhugyu <planetaryrotationwastaken1@gmail.com>
2025-04-02 05:56:48 -0700

    use hasTable and createTable instead of createTableIfNotExists

--- migrations/20250401154824_initial.js
+++ migrations/20250401154824_initial.js
@@ -4,66 +4,101 @@
  */
 exports.up = function(knex) {
     return knex.schema
-        .createTableIfNotExists("prompts", (table) => {
-            table.string("name").notNullable();
-            table.text("content").notNullable();
-            table.string("author_id").notNullable();
-            table.string("author_username").notNullable();
-            table.string("author_avatar");
-            table.timestamp("created_at").defaultTo(knex.fn.now());
-            table.timestamp("updated_at").defaultTo(knex.fn.now());
-            table.timestamp("published_at");
-            table.string("description").notNullable().defaultTo("No description provided.");
-            table.boolean("published").notNullable().defaultTo(false);
-            table.boolean("nsfw").notNullable().defaultTo(false);
-            table.boolean("default").notNullable().defaultTo(false);
-            table.json("api_parameters").notNullable().defaultTo("{}");
+        .hasTable("prompts").then((exists) => {
+            if (!exists) {
+                console.log("Creating table: prompts");
+                return knex.schema.createTable("prompts", (table) => {
+                    table.string("name").notNullable();
+                    table.text("content").notNullable();
+                    table.string("author_id").notNullable();
+                    table.string("author_username").notNullable();
+                    table.string("author_avatar");
+                    table.timestamp("created_at").defaultTo(knex.fn.now());
+                    table.timestamp("updated_at").defaultTo(knex.fn.now());
+                    table.timestamp("published_at");
+                    table.string("description").notNullable().defaultTo("No description provided.");
+                    table.boolean("published").notNullable().defaultTo(false);
+                    table.boolean("nsfw").notNullable().defaultTo(false);
+                    table.boolean("default").notNullable().defaultTo(false);
+                    table.json("api_parameters").notNullable().defaultTo("{}");
+                });
+            }
         })
-        .createTableIfNotExists("todos", (table) => {
-            table.string("user").notNullable();
-            table.string("name").notNullable();
-            table.integer("item").notNullable();
-            table.string("text").notNullable();
-            table.boolean("completed").notNullable().defaultTo(false);
-        })
-        .createTableIfNotExists("configs", (table) => {
-            table.string("guild").notNullable();
-            table.string("key").notNullable();
-            table.json("value").notNullable();
-            table.string("category").notNullable();
-        })
-        .createTableIfNotExists("queues", (table) => {
-            table.string("guild").notNullable();
-            table.string("type").notNullable();
-            table.integer("index");
-            table.string("url");
-            table.string("title");
-            table.integer("length");
-            table.string("videoId");
-            table.string("name");
-            table.string("key");
-            table.string("value");
-        })
-        .createTableIfNotExists("sounds", (table) => {
-            table.string("guild");
-            table.string("user");
-            table.string("name").notNullable();
-            table.string("path").notNullable();
-            table.timestamp("created_at").defaultTo(knex.fn.now());
-        })
-        .createTableIfNotExists("updates", (table) => {
-            table.integer('update').primary().notNullable();
-            table.string('text').notNullable();
-            table.timestamp('time').defaultTo(knex.fn.now());
-            table.string('message_id');
-            table.boolean('major').notNullable().defaultTo(false);
-        })
-        .createTableIfNotExists('statistics', (table) => {
-            table.string('type').notNullable();
-            table.string('name');
-            table.integer('value');
-            table.json('times');
-        })
+        .then(() => knex.schema.hasTable("todos").then((exists) => {
+            if (!exists) {
+                console.log("Creating table: todos");
+                return knex.schema.createTable("todos", (table) => {
+                    table.string("user").notNullable();
+                    table.string("name").notNullable();
+                    table.integer("item").notNullable();
+                    table.string("text").notNullable();
+                    table.boolean("completed").notNullable().defaultTo(false);
+                });
+            }
+        }))
+        .then(() => knex.schema.hasTable("configs").then((exists) => {
+            if (!exists) {
+                console.log("Creating table: configs");
+                return knex.schema.createTable("configs", (table) => {
+                    table.string("guild").notNullable();
+                    table.string("key").notNullable();
+                    table.json("value").notNullable();
+                    table.string("category").notNullable();
+                });
+            }
+        }))
+        .then(() => knex.schema.hasTable("queues").then((exists) => {
+            if (!exists) {
+                console.log("Creating table: queues");
+                return knex.schema.createTable("queues", (table) => {
+                    table.string("guild").notNullable();
+                    table.string("type").notNullable();
+                    table.integer("index");
+                    table.string("url");
+                    table.string("title");
+                    table.integer("length");
+                    table.string("videoId");
+                    table.string("name");
+                    table.string("key");
+                    table.string("value");
+                });
+            }
+        }))
+        .then(() => knex.schema.hasTable("sounds").then((exists) => {
+            if (!exists) {
+                console.log("Creating table: sounds");
+                return knex.schema.createTable("sounds", (table) => {
+                    table.string("guild");
+                    table.string("user");
+                    table.string("name").notNullable();
+                    table.string("path").notNullable();
+                    table.timestamp("created_at").defaultTo(knex.fn.now());
+                });
+            }
+        }))
+        .then(() => knex.schema.hasTable("updates").then((exists) => {
+            if (!exists) {
+                console.log("Creating table: updates");
+                return knex.schema.createTable("updates", (table) => {
+                    table.integer('update').primary().notNullable();
+                    table.string('text').notNullable();
+                    table.timestamp('time').defaultTo(knex.fn.now());
+                    table.string('message_id');
+                    table.boolean('major').notNullable().defaultTo(false);
+                });
+            }
+        }))
+        .then(() => knex.schema.hasTable("statistics").then((exists) => {
+            if (!exists) {
+                console.log("Creating table: statistics");
+                return knex.schema.createTable("statistics", (table) => {
+                    table.string('type').notNullable();
+                    table.string('name');
+                    table.integer('value');
+                    table.json('times');
+                });
+            }
+        }))
         .then(() => {
             console.log("Tables created successfully");
         })
