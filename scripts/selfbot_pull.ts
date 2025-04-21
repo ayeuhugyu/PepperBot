ade90bd8820b85f7eae0433a68be20731ae43f22
ayeuhugyu <planetaryrotationwastaken1@gmail.com>
2025-04-20 22:06:58 -1000

    selfbot pull now fixes bot.ts and installs libraries and shit

--- scripts/selfbot_pull.ts
+++ scripts/selfbot_pull.ts
@@ -3,9 +3,9 @@ import * as fs from "fs";
 import * as path from "path";
 import shell from "shelljs";
 
-shell.exec("git stash")
-
+shell.exec("git reset --hard")
 shell.exec("bun pull");
+shell.exec("bun install discord.js-selfbot-v13");
 
 const directory = "src"; // specify your directory here
 function processFile(filePath: string) {
@@ -41,4 +41,61 @@ function traverseDir(dir: string) {
 }
 traverseDir(directory);
 
-shell.exec("git stash pop")
+const mainFileContent = `import { Client } from 'discord.js-selfbot-v13';
+import { config } from 'dotenv';
+import * as log from './lib/log';
+import { Theme, getThemeEmoji } from './lib/theme';
+import fs from "fs";
+import { listen } from './web/index';
+config();
+
+export const client = new Client();
+
+const eventFiles = fs
+    .readdirSync("src/events")
+for (const file of eventFiles) {
+    (async () => {
+        const event = await import(\`./events/\${file}\`);
+        client.on(event.default.name, event.default.execute);
+    })();
+}
+
+client.once('ready', async () => {
+    log.info(\`logged in as \${client.user?.tag}\`);
+
+    // web server starting has been moved to index.ts because in here it would start one for every single shard
+
+    const channel = await client.channels.fetch("1312566483569741896").catch(() => {});
+    if (channel) {
+        if ('send' in channel) {
+            channel.send(\`it's pepper time \${getThemeEmoji(Theme.CURRENT)}\`);
+        }
+    }
+});
+
+async function init() {
+    log.info("logging into discord...");
+    try {
+        await client.login(process.env.DISCORD_TOKEN);
+        client.user?.setActivity("pepper whisperers.", { type: "LISTENING" });
+    } catch (err) {
+        log.error("failed to login into discord. wifi down? token invalid?");
+        log.error(err);
+        process.exit(1);
+    }
+}
+await init();
+process.env.DISCORD_CLIENT_ID = client.user?.id || "0";
+//await listen(client);
+
+process.on("warning", log.warn);
+["unhandledRejection", "uncaughtException"].forEach((event) => {
+    process.on(event, (err) => {
+        log.fatal(\`[PEPPERCRITICAL] bot.ts errored on \${event}: \`);
+        log.fatal(err);
+        console.error(err) // incase of the stupid fucking combined error bullshit discordjs returns for embed errors
+        process.exit(1);
+    });
+})`
+
+fs.writeFileSync("src/bot.ts", mainFileContent, "utf8");
