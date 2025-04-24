8fa871977278d6d37187f38f7020a3a8c4b43671
ayeuhugyu <72027312+ayeuhugyu@users.noreply.github.com>
2025-04-24 18:17:09 +0000

    p/echo

---
+++ src/commands/echo.ts
@@ -0,0 +1,50 @@
+import { Command, CommandAccess, CommandInvoker, CommandOption, CommandResponse } from "../lib/classes/command";
+import * as action from "../lib/discord_action";
+import { getArgumentsTemplate, GetArgumentsTemplateType, CommandAccessTemplates } from "../lib/templates";
+import { CommandTag, InvokerType, CommandOptionType } from "../lib/classes/command_enums";
+
+const command = new Command(
+    {
+        name: 'echo',
+        description: 'echoes the text you send it',
+        long_description: 'echoes the text you send it. this command is meant to be used for piping.',
+        tags: [CommandTag.Utility, CommandTag.TextPipable],
+        pipable_to: [CommandTag.TextPipable],
+        options: [
+            new CommandOption({
+                name: 'text',
+                description: 'text to echo',
+                long_description: 'the text to echo; may be piped',
+                type: CommandOptionType.String,
+                required: true
+            })
+        ],
+        access: CommandAccessTemplates.public,
+        input_types: [InvokerType.Message],
+        example_usage: "p/echo hello world",
+        aliases: ["say"]
+    },
+    getArgumentsTemplate(GetArgumentsTemplateType.SingleStringWholeMessage, ["text"]),
+    async function execute ({ invoker, args, guild_config, piped_data, will_be_piped }) {
+        const text = args.text || piped_data?.data?.input_text;
+        if (!text) {
+            await action.reply(invoker, { content: "no text provided", ephemeral: guild_config.other.use_ephemeral_replies });
+            return new CommandResponse({
+                error: true,
+                message: "no text provided",
+            });
+        }
+        if (will_be_piped) {
+            action.reply(invoker, { content: "piped text", ephemeral: guild_config.other.use_ephemeral_replies });
+            return new CommandResponse({
+                pipe_data: {
+                    input_text: text
+                }
+            });
+        }
+
+        const message = await action.reply(invoker, { content: text, ephemeral: guild_config.other.use_ephemeral_replies });
+    }
+);
+
+export default command;
