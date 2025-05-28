35aef30f6aa6af4c724114c9dbbd188e639bd4d4
ayeuhugyu <72027312+ayeuhugyu@users.noreply.github.com>
2025-05-28 16:10:46 +0000

    updated slang table

--- src/lib/gpt.ts
+++ src/lib/gpt.ts
@@ -296,42 +296,45 @@ const discordFormattingTable = [
 ]
 const discordFormattingColumns = ["Discord's Original Format", "Reformatted Version", "Description"];
 
-const slangTable = [
-    ["sybau", "shut your bitch ass up", "Basically equivalent to \"shut up\"; bitch is occasionally replaced with other insults (ex. synau using racist term for black people instead of bitch)."],
-    ["ts", "this shit", "This has a running joke of being equal to \"this\" instead of \"this shit\" or various other things, but it always will mean \"this shit\"."],
-    ["pmo", "pisses me off", "Often accompanied by ts. There is another running joke of just spamming \"ts\" and \"pmo\" in the same sentence over and over."],
-    ["gng", "gang", "Can also mean \"goodnight girl\", but this has fallen out of practice."],
-    ["808", "crashout", "To 808 or to \"crash out\" basically means to get mad or angry over something and throw a fit."],
-    ["js", "just", "Not as common as other ones. Can also mean \"just saying\""],
-    ["abt", "about", "Also not as common as other ones."],
-    ["icl", "i cant lie", "Can either be equivalent to \"i dont care\" or \"not gonna lie\", depeneds on context."],
-    ["h8", "hate", "Again, not as common."],
-    ["slime", "hurt", "To slime someone is to hurt or kill or otherwise do bad things to."],
-    ["nger", "(racist term for black people)", "I'm not black so i'm not gonna write it but I'm sure you understand what I mean. This is generally considered less formal and not as bad as the hard R version."],
-    ["🥷 (or just the word ninja)", "nger", "Sometimes on rare occasion used to mean that."],
-    ["tt", "tiktok", "Self explanatory."],
-    ["lowk", "low key", "Self explanatory."],
-    ["highk", "high key", "Equivalent to lowk. Some people just prefer highkey instead of lowkey."],
-    ["sm", "so much", "Self explanatory."],
-    ["dtf", "down to fuck?", "Not used commonly. Fuck in this context being used as an action."],
-    ["tuff", "thats so cool or tough", "Self explanatory. \"that's tuff\" is common."],
-    ["chopped", "ugly", "Does not always mean ugly in terms of looks."],
-    ["huzz", "hoes (sexual definition)", "Self explanatory. Often combined with other words, such as \"chuzz\" meaning \"chopped huzz\""],
-    ["😭", "funny", "means kindof the opposite of what you'd expect, often appended to messages to indicate that the thing is extremely funny."],
-    ["mb", "my bad", "Equivalent to saying sorry. Can also be rarely seen as \"mb all\", meaning \"my bad all\", a phrase which stems from a video game."],
-    ["tapped in", "Paying attention to or partaking in or in agreement with", "example: \"are you tapped into the Document?\" -> \"have you seen the document's contents?\""],
-    ["lock in", "hyperfocused", "To be locked in on something is to be hyperfocused on that thing, to be exceptionally good at it temporarily."],
-    ["peak", "really really cool", "Can also be used sarcastically."],
-    ["holy peak", "exceptionally really really really cool", "An intensified version of peak."],
-    ["peam", "peak", "A misspelling of peak that has become a popular term. It means the exact same."],
-    ["holy peam", "holy peak", "Once again, a misspelling of holy peak that has become a popular term. It means the exact same."],
-    ["elite ball knowledge", "secretive thing", "Usually used to refer to something as being \"elite ball knowledge\", ex. \"that document is elite ball knowledge\". This means that the document is secretive or not well known to anyone."],
-    ["fn", "fortnite OR fuck nger", "Exactly as defined. Depends on context, but usually the second one."],
-    ["goon", "masturbate", "Can also have suffixes added; gooning -> masturbating, gooner -> masturbator; goonable -> masturbatable. In more of a joking way than actually using the word \"masturbating\"."],
-    ["cracking", "have sex with", "To crack something is to have sex with it. ex. \"We will NOT be cracking them.\""],
-]
+function examplify(...examples: string[]): string {
+    return examples.map(example => `\"${example}\"`).join(", ");
+}
 
-const slangTableColumns = ["Term", "Definition", "Additional Notes"]
+const slangTable = [
+    ["sybau", "shut your bitch ass up", "Basically equivalent to \"shut up\"; bitch is occasionally replaced with other insults (ex. synau using racist term for black people instead of bitch).", examplify("dude sybau")],
+    ["ts", "this shit", "This has a running joke of being equal to \"this\" instead of \"this shit\" or various other things, but it always will mean \"this shit\".", examplify("i hate ts", "ts pmo", "ts thing", "why does ts thing do that")],
+    ["pmo", "pisses me off", "Often accompanied by ts. There is another running joke of just spamming \"ts\" and \"pmo\" in the same sentence over and over.", examplify("you pmo", "this pmo", "don't pmo", "ts pmo")],
+    ["gng", "gang", "N/A", examplify("yo gng", "wassup gng", "gng wtf")],
+    ["808", "crash out", "To 808 or to \"crash out\" basically means to get mad or angry over something and throw a fit.", examplify("stop 808ing", "im gonna 808", "i'm about to crash the fuck out.", "i'm gonna crash out")],
+    ["js", "just", "Not as common as other ones. Can also mean \"just saying\"", examplify("i js did that")],
+    ["abt", "about", "Also not as common as other ones.", examplify("i'm abt to do it")],
+    ["icl", "i cant lie", "Can either be equivalent to \"i dont care\" or \"not gonna lie\", depeneds on context. It almost always preceeds a statement.", examplify("icl ts sucks", "icl, it just doesn't matter")],
+    ["h8", "hate", "Again, not as common.", examplify("i h8 you")],
+    ["slime", "hurt", "To slime someone is to hurt or kill or otherwise do bad things to.", examplify("yeah im gonna slime you", "you're gonna get slimed")],
+    ["tt", "tiktok", "Can refer to the platform itself or a video on the platform.", examplify("js watched a tt", "i love tt")],
+    ["lowk", "low key", "Nearly meaningless prefix added onto stuff. Many people still prefer to use the full word rather than the abbreviation.", examplify("lowk ts is amazing", "lowkey what the fuck bro")],
+    ["highk", "high key", "Equivalent to lowk. Some people just prefer highkey instead of lowkey. This is abbreviated less often.", examplify("its highk awful")],
+    ["sm", "so much", "Self explanatory.", examplify("i hate ts sm")],
+    ["dtf", "down to fuck?", "Not used commonly. Fuck in this context being used as an action.", examplify("you dtf?")],
+    ["tuff", "thats so cool or tough", "Self explanatory. \"that's tuff\" is common. It's also common for it to be used entirely by itself.", examplify("ts is tuff", "thats tuff", "tuff")],
+    ["asf", "as fuck", "Used as a prefix to strengthen something or refer to something referring to another thing. Can also be written as \"ass mf\" if referring to a person.", examplify("i hate that stupid ass mf", "im mad asf")],
+    ["ur", "your", "Sometimes may also refer to you're, but usually its your.", examplify("i love ur shirt")],
+    ["chopped", "ugly", "Does not always mean ugly in terms of looks.", examplify("they're chopped asf", "chopped ass mf")],
+    ["huzz", "hoes (sexual definition)", "Self explanatory. Often combined with other words, such as \"chuzz\" meaning \"chopped huzz\"", examplify("meeting with the huzz tonight", "the chuzz sucks")],
+    ["😭", "funny", "means kindof the opposite of what you'd expect, often appended to messages to indicate that the thing is extremely funny. Might also be used entirely by itself in response to something.", examplify("dude wtf 😭😭", "😭", "don't be doin that 😭")],
+    ["mb", "my bad", "Equivalent to saying sorry (usually more jokingly though, not to be used as an actual apology). Sometimes multiple b's are appended to the end. Can also be rarely seen as \"mb all\", meaning \"my bad all\", a phrase which stems from a video game. \"mb all\" will pretty much only be used entirely by itself.", examplify("oh mb", "mb gng", "mb", "mb all", "oops mbbbbb")],
+    ["tapped in", "Paying attention to or partaking in or in agreement with", "example: \"are you tapped into the Document?\" -> \"have you seen the document's contents?\", or other example: \"i'm tapped in to peak\" -> \"i'm playing peak right now\" (or in this situation might not have been playing, but something related to peak). A third example: \"i'm tapped in to Monster, 2004.\" -> \"i'm currently watching the movie Monster, 2004.\"", examplify("we're all tapping in to the document", "are you tapped in to jol?")],
+    ["lock in", "hyperfocused", "To be locked in on something is to be hyperfocused on that thing, to be exceptionally good at it temporarily. Usually does not refer to the thing to be locked in to, simply stating that the user is locked in.", examplify("i'm locked in.", "hold on i gotta lock in")],
+    ["peak", "really really cool", "Can also be used sarcastically.", examplify("this is peak", "it's peak.", "i'm busy playing peak rn", "peak")],
+    ["holy peak", "exceptionally really really really cool", "An intensified version of peak. Usually is the only thing in the message.", examplify("holy peak")],
+    ["peam", "peak", "A misspelling of peak that has become a popular term. It means the exact same.", examplify("this is peam", "peam")],
+    ["holy peam", "holy peak", "Once again, a misspelling of holy peak that has become a popular term. It means the exact same.", examplify("holy peam")],
+    ["elite ball knowledge", "secretive thing", "Usually used to refer to something as being \"elite ball knowledge\", ex. \"that document is elite ball knowledge\". This means that the document is secretive or not well known to anyone. Often is used without referring directly to what the thing is.", examplify("elite ball knowledge", "dude that document is elite ball knowledge")],
+    ["goon", "masturbate", "Can also have suffixes added; gooning -> masturbating, gooner -> masturbator; goonable -> masturbatable. In more of a joking way than actually using the word \"masturbating\".", examplify("i'm gooning", "i'm a gooner", "this is goonable", "we will not be gooning to that bro 😭")],
+    ["cracking", "have sex with", "To crack something is to have sex with it. ex. \"We will NOT be cracking them.\"", examplify("we will not be cracking them", "we will be cracking them")],
+    ["WE", "N/A", "When referring to doing something (usually in a joking manner), it is common to use an all caps \"WE\", often followed soon after by \"yes, WE\" to reaffirm that EVERYONE will be doing whatever that thing is. This is never used seriously.", examplify("WE will be gooning tonight. yes, WE.", "WE are going to fail.", "WE will ALL be hopping on. yes, WE.")]
+]
+const slangTableColumns = ["Term", "Definition", "Additional Notes", "Example Usage"]
 
 const botPromptContent = `
 # Identity
