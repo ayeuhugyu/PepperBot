ddee361115b6864b072e325ee1c4bcff1d635d9b
ayeuhugyu <planetaryrotationwastaken1@gmail.com>
2025-06-30 10:31:55 -0700

    p/gpt configure

--- src/commands/conversation.ts
+++ src/commands/conversation.ts
@@ -1,12 +1,303 @@
-import { ApplicationCommandOptionType, ApplicationCommandType, ChannelType, Collection, GuildMember, Message, StageChannel, VoiceChannel } from "discord.js";
-import { Command, CommandAccess, CommandInvoker, CommandOption, CommandResponse } from "../lib/classes/command";
+import { ApplicationCommandOptionType, ApplicationCommandType, ChannelType, Collection, GuildMember, Message, StageChannel, TextInputStyle, VoiceChannel } from "discord.js";
+import { Command, CommandAccess, CommandInvoker, CommandOption, CommandResponse, FormattedCommandInteraction } from "../lib/classes/command";
 import * as action from "../lib/discord_action";
 import { CommandAccessTemplates, getArgumentsTemplate, GetArgumentsTemplateType } from "../lib/templates";
-import { getConversation, conversations, GPTFormattedCommandInteraction } from "../lib/gpt/main";
+import { getConversation, conversations, GPTFormattedCommandInteraction, Conversation } from "../lib/gpt/main";
 import { Model, Models as models } from "../lib/gpt/models";
 import { textToAttachment } from "../lib/attachment_manager";
 import { CommandTag, SubcommandDeploymentApproach, CommandOptionType, InvokerType } from "../lib/classes/command_enums";
 import { DiscordAnsi as ansi } from "../lib/discord_ansi";
+import { ActionRow, Button, ButtonStyle, Container, Section, Separator, TextDisplay, TextInput } from "../lib/classes/components";
+
+function serializeModel(model: Model): string {
+    return `${ansi.bold(ansi.green(model.name))} ${ansi.gray(`(${model.provider})`)}\n` +
+        `${ansi.gray(model.description)}\n` +
+        `${ansi.blue("capabilities: ")}${model.capabilities.join(', ')}` +
+        ((model.whitelist && model.whitelist.length > 0) ? `\n${ansi.blue("whitelist: ")}${model.whitelist.join(', ')}` : '');
+}
+
+function getAPIParametersButtons(conversation: Conversation, disabled: boolean = false) {
+    const model = findModelByName(conversation.model.name);
+    const templateAPIParameters = model?.parameters?.reduce<Record<string, any>>((acc, param) => {
+        acc[param.key] = param.default || 0; // default to 0 if no default is provided
+        return acc;
+    }, { model: conversation.model.name }) || { model: conversation.model.name };
+    const apiParameterButtons: ActionRow[] = []
+    Object.keys(templateAPIParameters).filter((key) => key !== "model").forEach((key) => {
+        const apiParameterButton = new Button({
+            style: ButtonStyle.Primary,
+            label: key,
+            custom_id: `edit_api_parameter_${key}`,
+            disabled: disabled
+        });
+        if (apiParameterButtons.length === 0 || apiParameterButtons[apiParameterButtons.length - 1].components.length >= 5) {
+            const row = new ActionRow({ components: [apiParameterButton] });
+            apiParameterButtons.push(row);
+        } else {
+            apiParameterButtons[apiParameterButtons.length - 1].components.push(apiParameterButton);
+        }
+    });
+    return apiParameterButtons;
+}
+
+interface APIParamVerifyResponse {
+    error: boolean;
+    message: string;
+}
+
+const defaultAPIParameters: Record<string, number> = {
+    temperature: 1,
+    top_p: 1,
+    presence_penalty: 0,
+    frequency_penalty: 0,
+    max_tokens: 4096
+};
+
+function verifyAPIParameter(parameter: string, value: number): APIParamVerifyResponse {
+    switch (parameter) {
+        case "temperature": {
+            if (value < 0 || value > 2) {
+                return { error: true, message: "temperature must be between 0 and 2" };
+            }
+            break;
+        }
+        case "top_p": {
+            if (value < 0 || value > 1) {
+                return { error: true, message: "top_p must be between 0 and 1" };
+            }
+            break;
+        }
+        case "presence_penalty": {
+            if (value < -2 || value > 2) {
+                return { error: true, message: "presence_penalty must be between -2 and 2" };
+            }
+            break;
+        }
+        case "frequency_penalty": {
+            if (value < -2 || value > 2) {
+                return { error: true, message: "frequency_penalty must be between -2 and 2" };
+            }
+            break;
+        }
+        case "max_tokens": {
+            if (value < 0 || value > 4096) {
+                return { error: true, message: "max_tokens must be between 0 and 4096" };
+            }
+            if (value % 1 !== 0) {
+                return { error: true, message: "max_tokens must be an integer, and cannot have a decimal place." };
+            }
+            break;
+        }
+        default: break;
+    }
+    return { error: false, message: "" };
+}
+
+const defaultModel = "gpt-4.1-nano"
+
+function getModelButtons(conversation: Conversation, disabled: boolean = false, authorId?: string) {
+    const modelButtons: ActionRow[] = []
+    Object.values(models).forEach((model) => {
+        const isDefault = model.name === defaultModel;
+        const isCurrent = conversation.model === model || (!conversation.model && isDefault);
+        const modelButton = new Button({
+            style: isDefault
+            ? ButtonStyle.Success
+            : isCurrent
+                ? ButtonStyle.Primary
+                : ButtonStyle.Secondary,
+            label: model.name,
+            custom_id: `edit_model_${model.name}`,
+            disabled: isCurrent || (model.whitelist && (model.whitelist.length > 0) && !model.whitelist.includes(authorId || "x")) || disabled
+        });
+        if (modelButtons.length === 0 || modelButtons[modelButtons.length - 1].components.length >= 5) {
+            const row = new ActionRow({ components: [modelButton] });
+            modelButtons.push(row);
+        } else {
+            modelButtons[modelButtons.length - 1].components.push(modelButton);
+        }
+    });
+    return modelButtons;
+}
+
+function embedConversation(conversation: Conversation): Container {
+    const container = new Container({
+        components: [
+            new TextDisplay({
+                content: `### conversation \`${conversation.id}\``
+            }),
+            new Separator(),
+            new Section({
+                components: [
+                    new TextDisplay({
+                        content: `API Parameters: \`\`\`ansi\n${(Object.values(conversation.api_parameters).length > 0) ? Object.entries(conversation.api_parameters).map(([k, v]) => {return ansi.blue(k) + ": " + v}).join("\n") : ansi.red("N/A")}\`\`\``
+                    }),
+                ],
+                accessory: new Button({
+                    custom_id: "configure_parameters",
+                    style: ButtonStyle.Success,
+                    label: "Configure"
+                })
+            }),
+            new Separator(),
+            new Section({
+                components: [
+                    new TextDisplay({
+                        content: `Model: \`\`\`ansi\n${serializeModel(conversation.model)}\`\`\``
+                    })
+                ],
+                accessory: new Button({
+                    custom_id: "configure_model",
+                    style: ButtonStyle.Success,
+                    label: "Configure"
+                })
+            })
+        ]
+    });
+    return container;
+}
+
+const configurecommand = new Command(
+    {
+        name: 'configure',
+        description: 'provides a graphical interface to configure your gpt conversation',
+        long_description: 'provides a graphical interface to configure your gpt conversation.',
+        tags: [CommandTag.Utility, CommandTag.AI],
+        example_usage: "p/conversation configure",
+        pipable_to: [],
+        options: [],
+        aliases: ["config", "build", "edit"]
+    },
+    getArgumentsTemplate(GetArgumentsTemplateType.DoNothing, []),
+    async function execute ({ invoker, args, guild_config, invoker_type }) {
+        let formattedInvoker: GPTFormattedCommandInteraction | CommandInvoker = invoker;
+        if (invoker_type === InvokerType.Interaction) {
+            formattedInvoker = Object.assign(invoker, {
+                author: invoker.author,
+                content: "",
+                attachments: new Collection(),
+            }) as unknown as GPTFormattedCommandInteraction;
+        }
+
+        const conversation = await getConversation(formattedInvoker as Message);
+
+        const sent = await action.reply(invoker, {
+            components: [embedConversation(conversation)],
+            components_v2: true,
+            ephemeral: guild_config.other.use_ephemeral_replies
+        });
+        if (!sent) return;
+
+        const collector = sent.createMessageComponentCollector();
+
+        collector.on("collect", async (interaction) => {
+            (interaction as FormattedCommandInteraction).author = interaction.user;
+            if (!conversation.users.includes(interaction.user)) {
+                await action.reply(interaction, {
+                    content: "you are not part of this conversation, and cannot configure it.",
+                    ephemeral: true
+                });
+                return;
+            }
+            if (interaction.customId === "configure_parameters") {
+                const apiParameterButtons = getAPIParametersButtons(conversation);
+                const reply = await action.reply(interaction, {
+                    components: [
+                        ...apiParameterButtons,
+                    ],
+                    ephemeral: guild_config.other.use_ephemeral_replies,
+                    fetchReply: true
+                });
+
+                if (!reply) return;
+
+                const apiparamCollector = reply.createMessageComponentCollector({
+                    time: 30 * 60 * 1000 // 30 minute timeout
+                });
+
+                apiparamCollector.on("collect", async (apiParameterInteraction) => {
+                    const key = apiParameterInteraction.customId.split("edit_api_parameter_")[1];
+                    const value = conversation.api_parameters[key];
+                    await apiParameterInteraction.showModal({
+                        custom_id: `api_parameter_modal`,
+                        title: `API Parameter: ${key}`,
+                        components: [
+                            new ActionRow({
+                                components: [
+                                    new TextInput({
+                                        custom_id: "api_parameter",
+                                        label: `API Parameter: ${key}`,
+                                        style: TextInputStyle.Short,
+                                        placeholder: `Enter the value of the API parameter`,
+                                        required: true,
+                                        value: value?.toString() || defaultAPIParameters[key]?.toString() || "unknown",
+                                    })
+                                ]
+                            }) as any
+                        ],
+                    });
+                    const submittedAPIParameter = await apiParameterInteraction.awaitModalSubmit({ time: 20 * 60 * 1000 });
+                    submittedAPIParameter.author = submittedAPIParameter.user;
+
+                    const userValue = parseFloat(submittedAPIParameter.fields.getTextInputValue("api_parameter"));
+
+                    const verifyResponse = verifyAPIParameter(key, userValue);
+                    if (verifyResponse.error) {
+                        action.reply(submittedAPIParameter as unknown as FormattedCommandInteraction, { content: verifyResponse.message, ephemeral: true });
+                        return;
+                    }
+
+                    conversation.api_parameters[key] = userValue;
+                    await action.reply(submittedAPIParameter as unknown as FormattedCommandInteraction, { content: `API parameter \`${key}\` set to \`${conversation.api_parameters[key]}\``, ephemeral: true });
+                    await action.edit(sent, {
+                        components: [embedConversation(conversation)],
+                        components_v2: true,
+                        ephemeral: guild_config.other.use_ephemeral_replies
+                    });
+                })
+            } else if (interaction.customId === "configure_model") {
+                const modelReply = await action.reply(interaction as unknown as FormattedCommandInteraction, {
+                    content: `select a model to use for this conversation. currently selected: \`${conversation.model.name || defaultModel}\``,
+                    components: getModelButtons(conversation, false, interaction.user.id),
+                    fetchReply: true,
+                    ephemeral: true,
+                });
+                if (!modelReply) return;
+                const modelCollector = modelReply.createMessageComponentCollector({ time: 30 * 60 * 1000 });
+                modelCollector.on("collect", async (modelInteraction) => {
+                    const modelName = modelInteraction.customId.split("edit_model_")[1];
+                    const model = findModelByName(modelName);
+                    if (!model) { // this should never happen
+                        await action.reply(modelInteraction as unknown as FormattedCommandInteraction, {
+                            content: `model '${modelName}' does not exist. something has gone terribly wrong.`,
+                            ephemeral: true,
+                        });
+                        return;
+                    }
+                    conversation.model = model;
+                    action.editReply(interaction as unknown as FormattedCommandInteraction, {
+                        content: `select a model to use for this prompt. currently selected: \`${conversation.model.name || defaultModel}\``,
+                        components: getModelButtons(conversation, false, interaction.user.id),
+                    });
+                    await action.edit(sent, {
+                        components: [embedConversation(conversation)],
+                        components_v2: true,
+                        ephemeral: guild_config.other.use_ephemeral_replies
+                    });
+                    modelInteraction.deferUpdate();
+                });
+                modelCollector.on("end", async () => {
+                    if (modelReply) {
+                        action.editReply(interaction as unknown as FormattedCommandInteraction, {
+                            content: `select a model to use for this prompt. currently selected: \`${conversation.model.name || defaultModel}\``,
+                            components: getModelButtons(conversation, true, interaction.user.id),
+                        });
+                    }
+                });
+            }
+        });
+    }
+);
 
 function findModelByName(name: string): Model | undefined {
     return Object.values(models).find(m =>
@@ -17,13 +308,6 @@ function findModelByName(name: string): Model | undefined {
         m.name.toLowerCase().includes(name.toLowerCase()) ||
         m.name.toLowerCase().replaceAll("-", "").includes(name.toLowerCase().replaceAll("-", ""))
     );
-}
-
-function serializeModel(model: Model): string {
-    return `${ansi.bold(ansi.green(model.name))} ${ansi.gray(`(${model.provider})`)}\n` +
-        `${ansi.gray(model.description)}\n` +
-        `${ansi.blue("capabilities: ")}${model.capabilities.join(', ')}` +
-        ((model.whitelist && model.whitelist.length > 0) ? `\n${ansi.blue("whitelist: ")}${model.whitelist.join(', ')}` : '');
 }
 
 function serializeModelParameters(parameters: Model['parameters']): string {
@@ -41,7 +325,7 @@ const modelcommand = new Command(
         name: 'model',
         description: 'set the model for your conversation',
         long_description: 'allows you to change which AI model your conversation uses',
-        tags: [],
+        tags: [CommandTag.AI],
         pipable_to: [],
         options: [
             new CommandOption({
@@ -369,7 +653,7 @@ const get = new Command(
         name: 'get',
         description: 'returns your gpt conversation',
         long_description: 'returns your gpt conversation. Append any text after the command to receive the full conversation output.',
-        tags: [CommandTag.Debug],
+        tags: [CommandTag.Debug, CommandTag.AI],
         example_usage: "p/conversation get full",
         pipable_to: [CommandTag.TextPipable],
         options: [
@@ -448,7 +732,7 @@ const command = new Command(
         example_usage: "p/conversation get",
         subcommands: {
             deploy:  SubcommandDeploymentApproach.Split,
-            list: [get, setparam, clear, modelcommand],
+            list: [get, setparam, clear, modelcommand, configurecommand],
         },
         aliases: ["conv", "gpt"],
         options: [],
