dbf525095a61d9ea9c33028046c19b22b3c9b460
ayeuhugyu <planetaryrotationwastaken1@gmail.com>
2025-08-20 21:03:38 -0400

    eslint config

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+// eslint.config.js for a modern TypeScript/Node/Discord.js project
+
+
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const importPlugin = require('eslint-plugin-import');
+
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+        ecmaVersion: 2021,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+  '@typescript-eslint/no-unused-vars': 'off',
+  'import/order': 'off',
+      'no-console': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    ignores: [
+      'dist/',
+      'node_modules/',
+      'cache/',
+      '*.d.ts',
+    ],
+  },
+];
