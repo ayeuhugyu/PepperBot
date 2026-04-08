c07964a220fa8e8087b0693103a190f30ada052e
ayeuhugyu <planetaryrotationwastaken1@gmail.com>
2026-04-08 06:50:41 -0700

    wip more conv stuff

--- src/lib/gpt/conversation.ts
+++ src/lib/gpt/conversation.ts
@@ -1,12 +1,30 @@
 import { User } from "discord.js";
 import { AnyModel, InferModelParameters, Model, ModelParameter } from "./modelTypes"
-import { Prompt, promptParameterTypings } from "./promptManager"
+import { AnyPrompt, Prompt, promptParameterTypings } from "./promptManager"
+import { getDefaultPrompt } from "./officialPrompts";
+import { models } from "./models";
+import * as log from "../log";
+import { randomId } from "../id";
 
-export class Conversation<M extends AnyModel> {
+const defaultPrompt = await getDefaultPrompt();
+
+export class Conversation<M extends AnyModel = typeof models['gpt-4.1-nano']> {
+    id: string = randomId("conv");
     messages: unknown; // TODO: make message types
-    prompt: Prompt<M, boolean, (string | undefined)>;
+    prompt: Prompt<M, boolean, (string | undefined)> = defaultPrompt! as Prompt<M>;
     promptParameterOverrides: Partial<InferModelParameters<typeof promptParameterTypings>> = {};
     modelParameterOverrides: Partial<InferModelParameters<M['parameters']>> = {};
-    model: M;
-    users: User[];
+    model: M = models['gpt-4.1-nano'] as unknown as M;
+    users: User[] = [];
+
+    setPrompt(prompt: AnyPrompt) {
+        const conv = this as Conversation<typeof prompt.model>;
+        conv.prompt = prompt;
+        conv.model = prompt.model;
+        // no need to update the prompt's model parameters, they'll be filtered later on their own.
+        log.info(`set prompt on conversation ${this.id}`);
+        log.debug(`set prompt on conversation ${this.id} to ${prompt.author.id}/${prompt.name}; full data:`)
+        log.debug(conv);
+        return conv;
+    }
 }
